17ef96de9aa2b36c382a12646cf56cad13527729
Gael Luguern <gael.luguer@gmail.com>
2025-12-24 12:14:21 +0100

    second page

--- script.js
+++ script.js
@@ -1,33 +1,108 @@
-/* SLIDER JS */
-    let index = 0;
-    const slides = document.getElementById('slides');
-    const totalSlides = slides.children.length;
+//current position
+var pos = 0;
+//number of slides
+var totalSlides = $('#slider-wrap ul li').length;
+//get the slide width
+var sliderWidth = $('#slider-wrap').width();
 
 
-    /* MENU JS */
-    const data = [
-      {
-        title: 'Présentation',
-        text: 'SAKOCHE est une marque moderne mettant en avant un design épuré et une identité forte.',
-        img: 'https://picsum.photos/500/350?4'
-      },
-      {
-        title: 'Produits',
-        text: 'Nos produits sont conçus avec soin pour allier style, qualité et durabilité.',
-        img: 'https://picsum.photos/500/350?5'
-      },
-      {
-        title: 'Valeurs',
-        text: 'Créativité, authenticité et exigence sont au cœur de l’univers SAKOCHE.',
-        img: 'https://picsum.photos/500/350?6'
-      }
-    ];
+$(document).ready(function(){
+	
+	
+	/*****************
+	 BUILD THE SLIDER
+	*****************/
+	//set width to be 'x' times the number of slides
+	$('#slider-wrap ul#slider').width(sliderWidth*totalSlides);
+	
+    //next slide 	
+	$('#next').click(function(){
+		slideRight();
+	});
+	
+	//previous slide
+	$('#previous').click(function(){
+		slideLeft();
+	});
+	
+	
+	
+	/*************************
+	 //*> OPTIONAL SETTINGS
+	************************/
+	//automatic slider
+	var autoSlider = setInterval(slideRight, 3000);
+	
+	//for each slide 
+	$.each($('#slider-wrap ul li'), function() { 
+	   //set its color
+	   var c = $(this).attr("data-color");
+	   $(this).css("background",c);
+	   
+	   //create a pagination
+	   var li = document.createElement('li');
+	   $('#pagination-wrap ul').append(li);	   
+	});
+	
+	//counter
+	countSlides();
+	
+	//pagination
+	pagination();
+	
+	//hide/show controls/btns when hover
+	//pause automatic slide when hover
+	$('#slider-wrap').hover(
+	  function(){ $(this).addClass('active'); clearInterval(autoSlider); }, 
+	  function(){ $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
+	);
+	
+	
 
-    function changeMenu(i) {
-      document.getElementById('menuTitle').textContent = data[i].title;
-      document.getElementById('menuDescription').textContent = data[i].text;
-      document.getElementById('menuImage').src = data[i].img;
+});//DOCUMENT READY
+	
 
-      document.querySelectorAll('.menu-buttons button').forEach(btn => btn.classList.remove('active'));
-      document.querySelectorAll('.menu-buttons button')[i].classList.add('active');
-    }
+
+/***********
+ SLIDE LEFT
+************/
+function slideLeft(){
+	pos--;
+	if(pos==-1){ pos = totalSlides-1; }
+	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 	
+	
+	//*> optional
+	countSlides();
+	pagination();
+}
+
+
+/************
+ SLIDE RIGHT
+*************/
+function slideRight(){
+	pos++;
+	if(pos==totalSlides){ pos = 0; }
+	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 
+	
+	//*> optional 
+	countSlides();
+	pagination();
+}
+
+
+
+	
+/************************
+ //*> OPTIONAL SETTINGS
+************************/
+function countSlides(){
+	$('#counter').html(pos+1 + ' / ' + totalSlides);
+}
+
+function pagination(){
+	$('#pagination-wrap ul li').removeClass('active');
+	$('#pagination-wrap ul li:eq('+pos+')').addClass('active');
+}
+		
+	
